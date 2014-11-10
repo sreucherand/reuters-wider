f06ef2ff9366d3189ebf66d77fdce6ff10cc557d
Clément Bardon <bardonclement@gmail.com>
2014-11-10 14:26:59 +0100

    Added Assets folder and working on a utils/variables file

---
+++ reuters-wider/Variables.h
@@ -0,0 +1,13 @@
+//
+//  Variables.h
+//  reuters-wider
+//
+//  Created by BARDON Clement on 10/11/2014.
+//  Copyright (c) 2014 Gobelins. All rights reserved.
+//
+
+#ifndef reuters_wider_Variables_h
+#define reuters_wider_Variables_h
+
+
+#endif
