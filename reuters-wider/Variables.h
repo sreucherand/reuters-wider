04a61e8d9b78bdd82c361aa8a7e0a65020603203
Clément Bardon <bardonclement@gmail.com>
2014-11-10 14:54:47 +0100

    No clue if this is going to work but I added custom fonts

--- reuters-wider/Variables.h
+++ reuters-wider/Variables.h
@@ -9,5 +9,13 @@
 #ifndef reuters_wider_Variables_h
 #define reuters_wider_Variables_h
 
+UIFont *CalibreRegular = [UIFont fontWithName:@"Calibre-Reg" size:18];
+UIFont *CalibreLight = [UIFont fontWithName:@"Calibre-Light" size:18];
+UIFont *LeituraRoman3 = [UIFont fontWithName:@"Leitura-Roman3" size:18];
+UIFont *LeituraRoman1 = [UIFont fontWithName:@"Leitura-Roman1" size:18];
+UIFont *LeituraItalic2 = [UIFont fontWithName:@"Leitura-Italic2" size:18];
+UIFont *ArcherXLight = [UIFont fontWithName:@"Archer-XLight" size:18];
+UIFont *ArcherThin = [UIFont fontWithName:@"Archer-Thin" size:18];
+UIFont *LeituraNewsRoman1 = [UIFont fontWithName:@"Leitura-NewsRoman1" size:18];
 
 #endif
