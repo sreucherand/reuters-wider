0be1c7ef48db247589e3e467401d8dbd77ef9fe4
Sylvain Reucherand <sylvainreucherand@gmail.com>
2014-11-10 10:22:26 +0100

    Initial Commit

---
+++ reuters-wider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reuters-wider
+//
+//  Created by REUCHERAND Sylvain on 10/11/2014.
+//  Copyright (c) 2014 Gobelins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ reuters-wider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  reuters-wider
+//
+//  Created by REUCHERAND Sylvain on 10/11/2014.
+//  Copyright (c) 2014 Gobelins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
