0a85c9758de14f5a627187b62b7385b9333396fc
Clément Bardon <bardonclement@gmail.com>
2014-11-26 14:56:22 +0100

    Started the variables related to articles, this is gonna be a mess

--- reuters-wider/Variables.h
+++ reuters-wider/Variables.h
@@ -24,9 +24,88 @@
 #define LEITURA_ROMAN_3_34 [UIFont fontWithName:@"Leitura-Roman3" size:34]
 #define LEITURA_ITALIC_1_15 [UIFont fontWithName:@"Leitura-Italic1" size:15]
 
+// ----
+// Article related
+// ----
+
+// ---- Fonts ----
+// Part title
+#define LEITURA_ROMAN_3_36 [UIFont fontWithName:@"Leitura-Roman3" size:36]
+// Duration part reading
+#define CALIBRE_LIGHT_16 [UIFont fontWithName:@"Calibre-Light" size:16]
+
+// Bloc meta
+#define CALIBRE_REG_11 [UIFont fontWithName:@"Calibre-Reg" size: 11]
+
+// Bloc teasing
+#define LEITURA_ROMAN_3_23 [UIFont fontWithName:@"Leitura-Roman3" size:23]
+
+// Bloc paragraph
+#define LEITURA_ROMAN_1_17 [UIFont fontWithName:@"Leitura-Roman1" size:17]
+
+// Bloc statement
+#define LEITURA_ITALIC_2_20 [UIFont fontWithName:@"Leitura-Italic2" size:20]
+
+// Bloc pov
+// Top part
+#define CALIBRE_LIGHT_14 [UIFont fontWithName:@"Calibre-Light" size:14]
+#define LEITURA_ROMAN_2_23 [UIFont fontWithName:@"Leitura-Roman2" size:23]
+// Bottom part
+#define ARCHER_THIN_38 [UIFont fontWithName:@"Archer-Thin" size: 38]
+
+// Bloc subtitle
+#define LEITURA_ROMAN_3_19 [UIFont fontWithName:@"Leitura-Roman3" size:19]
+
+// Bloc figure
+// Name
+#define LEITURA_ROMAN_2_32 [UIFont fontWithName:@"Leitura-Roman2" size:32]
+// Date
+#define LEITURA_ROMAN_3_16 [UIFont fontWithName:@"Leitura-Roman3" size:16]
+#define LEITURA_ROMAN_1_16 [UIFont fontWithName:@"Leitura-Roman1" size:16]
+
+// Bloc aside
+#define CALIBRE_LIGHT_17 [UIFont fontWithName:@"Calibre-Light" size:17]
+
+// Bloc keywords
+#define CALIBRE_REG_15 [UIFont fontWithName:@"Calibre-Reg" size:15]
+
+// Bloc transition
+#define LEITURA_ITALIC_2_38 [UIFont fontWithName:@"Leitura-Italic2" size:38]
+// ---- End fonts ----
+
+// ---- Colors ----
+// Part title
+#define DARKEST_GREY_COLOR [UIColor colorWithRed:(18.0f/255.0f) green:(18.0f/255.0f) blue:(18.0f/255.0f) alpha:1.0f]
+#define BLUE_COLOR [UIColor colorWithRed:(74.0f/255.0f) green:(76.0f/255.0f) blue:(159.0f/255.0f) alpha:1.0f]
+
+// Bloc meta
+#define LIGHT_BLUE_COLOR [UIColor colorWithRed:(146.0f/255.0f) green:(148.0f/255.0f) blue:(197.0f/255.0f) alpha:1.0f]
+
+// Bloc paragraph
+#define GREY_COLOR [UIColor colorWithRed:(67.0f/255.0f) green:(67.0f/255.0f) blue:(67.0f/255.0f) alpha:1.0f]
+
+// Bloc figure
+// BG
+#define BLUE_GREY_COLOR [UIColor colorWithRed:(235.0f/255.0f) green:(236.0f/255.0f) blue:(243.0f/255.0f) alpha:1.0f]
+// Txt
+#define DARK_GREY_COLOR [UIColor colorWithRed:(42.0f/255.0f) green:(42.0f/255.0f) blue:(42.0f/255.0f) alpha:1.0f]
+#define COOL_GREY_COLOR [UIColor colorWithRed:(80.0f/255.0f) green:(80.0f/255.0f) blue:(80.0f/255.0f) alpha:1.0f]
+
+// Bloc transition
+// BG
+#define LIGHT_BLUE_BG [UIColor colorWithRed:(202.0f/255.0f) green:(210.0f/255.0f) blue:(239.0f/255.0f) alpha:1.0f]
+// Txt
+#define DARK_BLUE_COLOR [UIColor colorWithRed:(27.0f/255.0f) green:(24.0f/255.0f) blue:(98.0f/255.0f) alpha:1.0f]
+
+// ---- End colors ----
+
+// ----
+// End article related
+// ----
 
 // Colors
 #define ISSUE_NR_COLOR [UIColor colorWithRed:(77.0f/255.0f) green:(77.0f/255.0f) blue:(77.0f/255.0f) alpha:1.0f]
 #define BLACK_COLOR [UIColor colorWithRed:(0.0f/255.0f) green:(0.0f/255.0f) blue:(0.0f/255.0f) alpha:1.0f]
+#define WHITE_COLOR [UIColor colorWithRed:(255.0f/255.0f) green:(255.0f/255.0f) blue:(255.0f/255.0f) alpha:1.0f]
 
 #endif
