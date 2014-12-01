05228ae55b75f805a51cdf63fa1e17d71e6d0869
Clément Bardon <bardonclement@gmail.com>
2014-12-01 20:38:04 +0100

    Roadblock. Coming back on this later.

---
+++ reuters-wider/CSPOVBlockTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CSPOVBlockTableViewCell.h
+//  reuters-wider
+//
+//  Created by Clément Bardon on 27/11/2014.
+//  Copyright (c) 2014 Gobelins. All rights reserved.
+//
+
+#import "CSAbstractArticleViewCellTableViewCell.h"
+
+@interface CSPOVBlockTableViewCell : CSAbstractArticleViewCellTableViewCell
+
+@end
