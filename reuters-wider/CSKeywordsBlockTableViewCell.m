dc079944cdda08d964d16d041b900d57ec60b824
Clément Bardon <bardonclement@gmail.com>
2015-01-06 14:33:12 +0100

    Bloc keywords is done

--- reuters-wider/CSKeywordsBlockTableViewCell.m
+++ reuters-wider/CSKeywordsBlockTableViewCell.m
@@ -8,8 +8,16 @@
 
 #import "CSKeywordsBlockTableViewCell.h"
 
-@implementation CSKeywordsBlockTableViewCell
+@interface CSKeywordsBlockTableViewCell()
 
+@property (weak, nonatomic) IBOutlet UIView *mainView;
+@property (weak, nonatomic) IBOutlet UIView *mainSuperView;
+
+
+@end
+
+
+@implementation CSKeywordsBlockTableViewCell
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
@@ -17,5 +25,45 @@
     // Drawing code
 }
 */
+
+- (void)awakeFromNib {
+    CALayer *topBorder = [CALayer layer];
+    
+    topBorder.frame = CGRectMake(20, 0, (CGRectGetWidth(self.mainSuperView.frame) - 50), 1);
+    topBorder.backgroundColor = LIGHT_GREY_BORDER_COLOR.CGColor;
+    
+    [self.mainSuperView.layer addSublayer:topBorder];
+}
+
+- (void)hydrateWithContentData:(NSDictionary *)data {
+    [super hydrateWithContentData:data];
+    
+    NSInteger fullSize = 0;
+    NSTextAttachment *icon = [[NSTextAttachment alloc] init];
+    icon.image = [UIImage imageNamed:@"iconBullet"];
+    
+    NSAttributedString *attachmentString = [NSAttributedString attributedStringWithAttachment:icon];
+    
+    [self.mainView layoutIfNeeded];
+    
+    for(int i = 0; i < [self.content.keywords count]; i++) {
+        NSMutableAttributedString *stringWithIcon= [[NSMutableAttributedString alloc] initWithAttributedString:attachmentString];
+        NSAttributedString *currentKeywordText = [[NSMutableAttributedString alloc] initWithString:[self.content.keywords[i] text]];
+        [stringWithIcon appendAttributedString:currentKeywordText];
+        
+        CGRect labelRect = CGRectMake (fullSize, 0, 80, CGRectGetHeight(self.mainView.frame));  // location of label in the window
+        UILabel *keywordLabel = [[UILabel alloc] initWithFrame: labelRect];
+        
+        keywordLabel.attributedText = stringWithIcon;
+        keywordLabel.clipsToBounds=YES;
+        keywordLabel.font = CALIBRE_REG_15;
+        keywordLabel.textColor = BLUE_COLOR;
+        
+        [self.mainView addSubview: keywordLabel];
+        
+        CGSize textSize = [[keywordLabel text] sizeWithAttributes:@{NSFontAttributeName:[keywordLabel font]}];
+        fullSize += textSize.width + 32;
+    }
+}
 
 @end
